bc0de5fc69676eed8ff1b05844e6b5e76bbf7c45
Aaron Fisher <fisher47@llnl.gov>
2017-02-25 14:43:21 -0800

    Use Unix LF EOL.

--- RayTools.py
+++ RayTools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Ray:
     def __init__(self, start, v):
@@ -26,3 +27,20 @@ class Ray:
 	tmid = 0.5*(tlo + thi)
 	self.end = self.start + tmid*self.v
 	
+
+class RayChain:
+    def __init__(self, start, v, zbound):
+        self.rays = [Ray(start, v)]
+        self.zbound = zbound
+        
+    def propagateThroughSurface(self, patch_grid, n_before, n_after):
+        self.rays[-1].setEndAtIntersection(patch_grid, self.zbound)
+        p = self.rays[-1].end
+        v_in = self.rays[-1].v
+        v_normal = patch_grid.getNormal(p[0],p[1])
+        
+        c = -np.linalg.dot(v_normal, v_in)
+        r = n_before / n_after
+        v_out = r*v_in + (r*c + math.sqrt(1. - r**2. * (1. - c**2.)))
+        self.rays.append(Ray(p, v_out))
+        
